4befb642809298953b79805ed23ad746fca911f0
root <tosinezekiel03@gmail.com>
2023-12-07 11:16:15 +0000

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -29,15 +29,15 @@ def do_deploy(archive_path):
 
     try:
         put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(archive_folder))
-        run("sudo tar -xzf /tmp/{} -C {}".format(archive_name,
+        run("mkdir -p {}".format(archive_folder))
+        run("tar -xzf /tmp/{} -C {}".format(archive_name,
                                             archive_folder))
-        run("sudo rm /tmp/{}".format(archive_name))
-        run("sudo mv {}/web_static/* {}".format(archive_folder,
+        run("rm /tmp/{}".format(archive_name))
+        run("mv {}/web_static/* {}".format(archive_folder,
                                                 archive_folder))
-        run("sudo rm -rf {}/web_static".format(archive_folder))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(archive_folder))
+        run("rm -rf {}/web_static".format(archive_folder))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(archive_folder))
         return True
     except Exception:
         return False
