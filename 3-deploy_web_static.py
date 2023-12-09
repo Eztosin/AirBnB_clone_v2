9ebe9b6a4cbb378a75fc1d7a29175571be72687c
root <tosinezekiel03@gmail.com>
2023-12-09 15:21:37 +0000

    Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 from fabric import task, Connection
 from datetime import datetime
+from fabric.operations import local as lcl
+
+env.hosts = ["54.90.26.239", "184.72.101.54"]
 
 
-env.hosts = ['54.90.26.239', '184.72.101.54']
-
-
+@task
 def do_pack():
     """
     Creates a compressed .tgz file from the contents of the
     web_static folder
     """
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    local("mkdir -p versions")
+    lcl("mkdir -p versions")
     path = "versions/web_static_{}.tgz".format(timestamp)
-    result = local("tar -cvzf {} web_static".format(path))
+    result = lcl("tar -cvzf {} web_static".format(path))
     if result.failed:
         return None
     return path
@@ -25,7 +26,7 @@ def deploy(c):
     """
     Packs and deploys the web_static folder to web servers
     """
-    archive_path = do_pack()
+    archive_path = c.do_pack()
     if not archive_path:
         return False
 
@@ -36,13 +37,13 @@ def deploy(c):
             release_folder = "/data/web_static/releases/web_static_{}/"
             .format(timestamp)
             conn.run('mkdir -p {}'.format(release_folder))
-            conn.run('tar -xzf /tmp/web_static_{}.tgz -C {}'
-                     .format(timestamp, release_folder))
+            conn.run('tar -xzf /tmp/web_static_{}.tgz -C {}'.
+                     format(timestamp, release_folder))
             conn.run('rm /tmp/web_static_{}.tgz'.format(timestamp))
             conn.run('mv {}web_static/* {}'.format(release_folder,
                                                    release_folder))
             conn.run('rm -rf {}web_static'.format(release_folder))
             conn.run('rm -rf /data/web_static/current')
-            conn.run('ln -s {} /data/web_static/current'
-                     .format(release_folder))
+            conn.run('ln -s {} /data/web_static/current'.
+                     format(release_folder))
     return True
