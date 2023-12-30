b3a2d8b4307f5d279c0b81868003d024992eb713
root <tosinezekiel03@gmail.com>
2023-12-30 12:18:51 +0000

    Python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""contains a script that starts a Flask web application"""
+from flask import Flask
+
+
+app = Flask("__name__")
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """displays a message"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """displays a message"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """displays “C” followed by the value of the text variable"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text):
+    """display “Python”, followed by the value of the text variable"""
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
