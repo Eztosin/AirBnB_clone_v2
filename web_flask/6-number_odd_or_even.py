573881a9d04d92b29339b15f79ed24e72688fcea
root <tosinezekiel03@gmail.com>
2024-01-04 23:33:43 +0000

    Odd or even?

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -42,9 +42,9 @@ def is_number(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-     """display a HTML page only if n is an integer"""
-     if type(n) is int:
-         return render_template('5-number.html', n=n)
+    """display a HTML page only if n is an integer"""
+    if type(n) is int:
+        return render_template('5-number.html', n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
